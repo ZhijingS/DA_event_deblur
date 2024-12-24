4a9bae3561f9b1f3218c2b4e2ba568285230da93
ZhijingS <113695874+ZhijingS@users.noreply.github.com>
2024-12-24 10:41:09 +0800

    Update test.py

--- basicsr/test.py
+++ basicsr/test.py
@@ -2,7 +2,7 @@ import logging
 import torch
 from os import path as osp
 import sys
-sys.path.insert(0,'D:/EFNet')
+sys.path.append('/code/DA_event_deblur-main')
 from basicsr.data import create_dataloader, create_dataset
 from basicsr.models import create_model
 from basicsr.train import parse_options
